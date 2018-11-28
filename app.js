74c6c3529c71370ce430f9c3a25ef2b4f051312b
Matt Sweeney <msweeney777@gmail.com>
2018-11-28 10:45:47 -0500

    Second commit

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: 'Address to fetch weather for',
+      string: true
+    }
+});
 
 request({
   url: 'http://www.mapquestapi.com/geocoding/v1/address?key=uuTltY0BG2CHeJgUkWzPEHHAlW3GzZyW&location=1301%20lombard%20street%20philadelphia',
