b9414732a7cd78f72f2529d68536c313d6ea7264
Matt Sweeney <msweeney777@gmail.com>
2018-11-29 16:47:53 -0500

    Seventh commit

--- app.js
+++ app.js
@@ -1,25 +1,40 @@
-const yargs = require('yargs');
+//const yargs = require('yargs');
 
-const geocode = require('./geocode/geocode.js');
+//const geocode = require('./geocode/geocode.js');
 
-const argv = yargs
-  .options({
-    a: {
-      demand: true,
-      alias: 'address',
-      describe: 'Address to fetch weather for',
-      string: true
-    }
-  })
+//const argv = yargs
+  //.options({
+    //a: {
+      //demand: true,
+      //alias: 'address',
+      //describe: 'Address to fetch weather for',
+      //string: true
+    //}
+  //})
 
-  .help()
-  .alias('help', 'h')
-  .argv;
+  //.help()
+  //.alias('help', 'h')
+  //.argv;
 
-geocode.geocodeAddress(argv.address, (errorMessage, results) => {
-  if(errorMessage) {
-    console.log(errorMessage);
+//geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+  //if(errorMessage) {
+    //console.log(errorMessage);
+  //} else {
+    //console.log(JSON.stringify(results, undefined, 2));
+  //}
+//});
+
+// 4d095d7c54f67904eae8d3dbf50e8543
+
+const request = require('request');
+
+request({
+  url: 'https://api.darksky.net/forecast/4d095d7c54f67904eae8d3dbf50e8543/43.126462,-70.92288',
+  json: true
+}, (error, response, body) => {
+  if(!error && response.statusCode === 200){
+    console.log(body.currently.temperature);
   } else {
-    console.log(JSON.stringify(results, undefined, 2));
+    console.log('Unable to fetch weather.');
   }
 });
