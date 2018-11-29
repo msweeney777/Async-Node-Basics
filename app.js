c79f6f00800c99966d321f0309d969559ec16736
Matt Sweeney <msweeney777@gmail.com>
2018-11-28 22:05:10 -0500

    Fifth commit

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-const request = require('request');
 const yargs = require('yargs');
+
+const geocode = require('./geocode/geocode.js');
 
 const argv = yargs
   .options({
@@ -15,20 +16,4 @@ const argv = yargs
   .alias('help', 'h')
   .argv;
 
-var encodedAddress = encodeURIComponent(argv.address);
-
-
-request({
-  url: `http://www.mapquestapi.com/geocoding/v1/address?key=uuTltY0BG2CHeJgUkWzPEHHAlW3GzZyW&location=${encodedAddress}`,
-  json: true
-}, (error, response, body) => {
-  if (error) {
-    console.log('Unable to connect to Google servers.');
-  } else if (body.results[0].locations[0].geocodeQualityCode === 'A1XAX') {
-    console.log('Unable to find that address.');
-  } else if (body.results[0].locations[0].geocodeQualityCode != 'A1XAX') {
-  console.log(`Address: ${body.results[0].providedLocation.location}`);
-  console.log(`Latitude: ${body.results[0].locations[0].latLng.lat}`);
-  console.log(`Longitude: ${body.results[0].locations[0].latLng.lng}`);
-  }
-});
+geocode.geocodeAddress(argv.address);
