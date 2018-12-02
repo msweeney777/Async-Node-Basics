bbf1b7724218a2e84009b2661eceaf90a8884fc6
Matt Sweeney <msweeney777@gmail.com>
2018-12-02 14:46:25 -0500

    Second commit

--- playground/promise.js
+++ playground/promise.js
@@ -1,7 +1,12 @@
 var somePromise = new Promise((resolve, reject) => {
-  resolve('Hey. It worked');
+  setTimeout(() => {
+    resolve('Hey. It worked');
+    //reject('Unable to fulfill promise');
+  }, 2500);
 });
 
 somePromise.then((message) => {
   console.log('Success: ', message)
-})
+}, (errorMessage) => {
+  console.log('Error: ', errorMessage);
+});
